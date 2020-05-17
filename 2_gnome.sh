1c654b8aa4def9347725f9ca6acbf23f4fa8ca4b
Dylan M. Taylor <dylan@dylanmtaylor.com>
2020-05-16 21:04:05 -0400

    Update 2_gnome.sh

--- 2_gnome.sh
+++ 2_gnome.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
 echo "Downloading and running base script"
-wget https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/2_base.sh
+wget https://raw.githubusercontent.com/dylanmtaylor/minimal-arch-linux/master/2_base.sh
 chmod +x 2_base.sh
 sh ./2_base.sh
 
 echo "Installing Gnome and a few extra apps"
-sudo pacman -S --noconfirm gnome gnome-tweaks gnome-usage gitg evolution gvfs-goa
+sudo pacman -S --noconfirm gnome gnome-tweaks gvfs-goa libreoffice-fresh
 
 echo "Enabling GDM"
 sudo systemctl enable gdm.service
 
-echo "Enabling automatic login"
-sudo tee -a /etc/gdm/custom.conf << END
-# Enable automatic login for user
-[daemon]
-AutomaticLogin=$USER
-AutomaticLoginEnable=True
-END
-
-echo "Downloading themes"
-# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/themes
-mkdir -p ~/.themes
-wget -P ~/.themes https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/themes/kali-themes.tar
-tar -xzf ~/.themes/kali-themes.tar -C ~/.themes
-rm -f ~/.themes/kali-themes.tar
-gsettings set org.gnome.desktop.interface gtk-theme theme-name
-
-echo "Downloading icon themes"
-# Kali themes source: https://gitlab.com/kalilinux/packages/kali-themes/-/tree/kali/master/share/icons
-mkdir -p ~/.icons
-wget -P ~/.icons https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/themes/kali-icons.tar
-tar -xzf ~/.icons/kali-icons.tar -C ~/.icons
-rm -f ~/.icons/kali-icons.tar
+echo "Installing yay"
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf yay-bin
 
 echo "Your setup is ready. You can reboot now!"
