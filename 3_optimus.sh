44ae3487a364d0d5c3eef2d34a6714a85a1c636e
Dylan M. Taylor <dylan@dylanmtaylor.com>
2020-05-22 21:59:55 -0400

    Create 3_optimus.sh

---
+++ 3_optimus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Installing Nvidia drivers and optimus-manager ..."
+yay -S --noconfirm gdm-prime optimus-manager nvidia-dkms
+
+echo "Enabling optimus-manager service"
+sudo systemctl enable --now optimus-manager
+
+echo "Writing udev rules"
+cat <<EOF | sudo tee /lib/udev/rules.d/80-nvidia-pm.rules
+cat /lib/udev/rules.d/80-nvidia-pm.rules
+# Remove NVIDIA USB xHCI Host Controller devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
+
+# Remove NVIDIA USB Type-C UCSI devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
+
+# Remove NVIDIA Audio devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
+
+# Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+
+# Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
+ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+EOF
+
+echo "Setting dynamic power management in nvidia.conf"
+echo "options nvidia "NVreg_DynamicPowerManagement=0x02" | sudo tee /etc/modprobe.d/nvidia.conf
